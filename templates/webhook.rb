2d208eb64841874679dac384ec3e251a2ee9eef6
Christian Pradelli <c.pradelli@sig2k.com>
2018-01-07 20:49:16 -0300

    .

--- templates/webhook.rb
+++ templates/webhook.rb
@@ -1,41 +1,40 @@
 require 'sinatra'
 require 'json'
 
-# User customization
-repo_puppetfile = "<%= @repo_puppetfile %>"
-<% if @repo_hieradata -%>
-repo_hierdata   = "<%= @repo_hieradata %>"
-<% end -%>
+#webhook_config_obj = JSON.parse(File.read((Dir.pwd) + "/webhook_config.json"))
 
-webhook_config_obj = JSON.parse(File.read((Dir.pwd) + "/webhook_config.json"))
+get '/payload' do
 
-post '/payload' do
+  "Hello World!"
 
-  content = request.body.read
-  logger.info("#{content}")
+  logger.info("paso por aca")
 
-  push = JSON.parse(request.body.read)
-  logger.info("json payload: #{push.inspect}")
 
-  repo_name   = push['repository']['name']
-  repo_ref    = push['ref']
-  ref_array   = repo_ref.split("/")
-  ref_type    = ref_array[1]
-  branchName  = ref_array[2]
-  logger.info("repo name = #{repo_name}")
-  logger.info("repo ref = #{repo_ref}")
-  logger.info("branch = #{branchName}")
+#  content = request.body.read
+#  logger.info("#{content}")
+
+#  push = JSON.parse(request.body.read)
+#  logger.info("json payload: #{push.inspect}")
+
+#  repo_name   = push['repository']['name']
+#  repo_ref    = push['ref']
+#  ref_array   = repo_ref.split("/")
+#  ref_type    = ref_array[1]
+#  branchName  = ref_array[2]
+#  logger.info("repo name = #{repo_name}")
+#  logger.info("repo ref = #{repo_ref}")
+#  logger.info("branch = #{branchName}")
 
   # Check if repo_name is 'puppetfile'
-  if repo_name == repo_puppetfile <% if @repo_hieradata %>|| repo_name == repo_hieradata <% end %>
-    logger.info("Deploy r10k for this environment #{branchName}")
-    deployEnv(branchName,webhook_config_obj)
-  else
-    module_name = repo_name.split('-').pop
-    logger.info("Deploy puppet module #{module_name}")
-    logger.info("Running for branch #{branchName}")
-    deployModule(module_name,webhook_config_obj)
-  end
+#  if repo_name == repo_puppetfile <% if @repo_hieradata %>|| repo_name == repo_hieradata <% end %>
+#    logger.info("Deploy r10k for this environment #{branchName}")
+#    deployEnv(branchName,webhook_config_obj)
+#  else
+#    module_name = repo_name.split('-').pop
+#    logger.info("Deploy puppet module #{module_name}")
+#    logger.info("Running for branch #{branchName}")
+#    deployModule(module_name,webhook_config_obj)
+#  end
 end
 
 # Some defines.
